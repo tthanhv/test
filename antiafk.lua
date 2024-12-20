9cd7c3fe55e4d69a28e8a5ec252a9238a17a7f16
TienThanh <caotienthanh924@gmail.com>
2024-12-20 19:55:57 +0700

    Update antiafk.lua

--- antiafk.lua
+++ antiafk.lua
@@ -2,4 +2,10 @@ local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:Connect(function()
     vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
     vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    game:GetService("StarterGui"):SetCore("SendNotification", { 
+        Title = "Anti AFK";
+        Text = "By HaXer19";
+        Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150";
+        Duration = 5; 
+    })
 end)
